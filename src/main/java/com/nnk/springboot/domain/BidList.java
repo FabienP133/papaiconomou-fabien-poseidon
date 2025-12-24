3382159c8af5b82937defa6f76030dfec90f9f09
Fabien <fabienppcnm@gmail.com>
2025-12-24 15:28:29 +0100

    commit initial

---
+++ src/main/java/com/nnk/springboot/domain/BidList.java
@@ -0,0 +1,14 @@
+package com.nnk.springboot.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import jakarta.validation.constraints.NotBlank;
+import java.sql.Date;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "bidlist")
+public class BidList {
+    // TODO: Map columns in data table BIDLIST with corresponding java fields
+}
